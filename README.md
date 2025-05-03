3f3ca4765817c33808145619955354d58bd0084e
Sebastine Odeh <sebastinesoacatp@gmail.com>
2025-05-03 07:22:05 +0100

    🚧 update readmes

---
+++ README.md
@@ -0,0 +1,4 @@
+# Strange Icons 🧪🚧
+
+This repository holds packages for strange icons icon packs with over 4000 icons in monotone and
+solid styles from [StrangeIcons](https://www.strangeicons.com/). 
--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-folder_names=("asklepios_icons" "freud_icons" "icon_pack" "nightingale_icons" "osler_icons" "turing_icons")
+folder_names=("asklepios_icons" "freud_icons" "icon_pack" "nightingale_icons" "osler_icons" "sandow_icons" "turing_icons")
 
 for folder in "${folder_names[@]}"
 do
